9b9764f59a1b5028f7509b59fcbf7ae2471fbff6
yar92zer <yar199199@gmail.com>
2024-07-15 20:07:13 +0400

    Initial commit

---
+++ src/test/java/org/example/habrtest/MainPageTest.java
@@ -0,0 +1,48 @@
+package org.example.habrtest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.habr.com/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void changeLogTest() {
+        WebElement userIcon = driver.findElement(By.cssSelector("svg[data-test-id='menu-toggle-user'] "));
+        userIcon.click();
+
+        WebElement rulesLink = driver.findElement(By.xpath("//*[contains(text(),'Правила сайта')]"));
+        rulesLink.click();
+
+        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Changelog')]")).isDisplayed(),"Changelog не найден");
+    }
+
+
+}
